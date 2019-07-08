bcbedfb5e32b4e47fe2dd55c139c85edf0e9d888
Taylor <2237221210@qq.com>
2019-07-08 23:42:00 +0800

    '增加分页js,封装好分页'

--- app/router.js
+++ app/router.js
@@ -19,6 +19,7 @@ module.exports = app => {
   router.get('/query_true_one',controller.studentController.queryPaginationOne);
   // 方法二(sequelize联合查询重点，待完善)
   router.get('/query_true_two',controller.studentController.queryPaginationTwo);
+  
   // 成绩表和学生表联合查询，假分页接口，一次性全部查（不推荐，只是简单提供一下处理思路）
   router.get('/query_false_one',controller.studentController.queryAllOne);
 };
