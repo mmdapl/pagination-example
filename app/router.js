90dd7cb61ca664977e1bff9a3011b2e96f5ffee8
Taylor <2237221210@qq.com>
2019-07-09 17:41:12 +0800

    整理前端代码，添加样式和版权说明

--- app/router.js
+++ app/router.js
@@ -19,7 +19,7 @@ module.exports = app => {
   // 成绩表和学生表联合查询，真分页接口，一页一页的查
   // 方法一
   router.get('/query_true_one',controller.studentController.queryPaginationOne);
-  // 方法二(sequelize联合查询重点，待完善)
+  // 方法二(sequelize联合查询重点，待完善).....(已完善)
   router.get('/query_true_two',controller.studentController.queryPaginationTwo);
   
   // 成绩表和学生表联合查询，假分页接口，一次性全部查（不推荐，只是简单提供一下处理思路）
